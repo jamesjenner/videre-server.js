0bdb3b6c7fa738aa3b9c9b5c36f2ab520fb86e5c
James Jenner <james.g.jenner@gmail.com>
2013-01-01 14:12:49 +1000

    Added copyright info

--- videre_server.js
+++ videre_server.js
@@ -1,3 +1,20 @@
+/*
+ * videre_server.js v0.1 alpha
+ *
+ * Copyright (c) 2012 James G Jenner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
+ */
 
 var videre_comms  = require('./videre-common/js/videre_comms.js');
 var vehicle       = require('./videre-common/js/vehicle.js');
