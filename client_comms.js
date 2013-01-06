0d8241395c0060ea46d521c35340ccdd077ca80d
James Jenner <james.g.jenner@gmail.com>
2013-01-06 22:43:27 +1000

    Rework to integrate client and server comms

--- client_comms.js
+++ client_comms.js
@@ -21,38 +21,159 @@ var vehicle       = require('./videre-common/js/vehicle.js');
 var videre_comms  = require('./videre-common/js/videre_comms.js');
 */
 
-var fs = require('fs');
+var EventEmitter = require('events').EventEmitter;
+var util         = require('util');
+var http         = require('http');
+var fs           = require('fs');
+var ws           = require('websocket').server;
 
 // load common js files shared with the videre client
 eval(fs.readFileSync('./videre-common/js/vehicle.js').toString());
 eval(fs.readFileSync('./videre-common/js/videre_comms.js').toString());
 
-var ws = require('websocket').server;
-var http = require('http');
+module.exports = ClientComms;
 
-var vehicles = new Array();
+util.inherits(ClientComms, EventEmitter);
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    reponse.writeHead(404);
-    reponse.end();
-});
+function ClientComms(options) {
+     EventEmitter.call(this);
 
-server.listen(9007, function() {
-    console.log((new Date()) + ' Server is listening on port 9007');
-});
+     options = options || {};
 
-var wsServer = new ws({
-    httpServer: server,
-    autoAcceptConnections: false
-});
+     this.vehiclesStoredOnServer = options.vehiclesStoredOnServer || true;
+     this.allowMultipleClients = options.allowMultipleClients || true;
+     this.allowAddVehicle = options.allowAddVehicle || true;
+     this.allowDeleteVehicle = options.allowDeleteVehicle || true;
+     this.allowUpdateVehicle = options.allowUpdateVehicle || true;
+}
+
+
+ClientComms.prototype.startClientServer = function() {
+    var httpServer = http.createServer(function(request, response) {
+        console.log((new Date()) + ' Received request for ' + request.url);
+        reponse.writeHead(404);
+        reponse.end();
+    });
+    var self = this;
+
+    httpServer.listen(9007, function() {
+        console.log((new Date()) + ' Server is listening on port 9007');
+    });
+
+    this.server = new ws({
+        httpServer: httpServer,
+        autoAcceptConnections: false
+    });
+
+    this.server.on('request', function(request) {processConnectionAttempt(self, request);});
+}
+
+ClientComms.prototype.sendVehicles = function(connection, vehicles) {
+    connection.send(Message.constructMessageJSON(MSG_VEHICLES, vehicles));
+}
+
+ClientComms.prototype.sendAddVehicle = function(vehicle) {
+    this.server.broadcast(Message.constructMessageJSON(MSG_ADD_VEHICLE, vehicle));
+}
+
+ClientComms.prototype.sendDeleteVehicle = function(vehicle) {
+    this.server.broadcast(Message.constructMessageJSON(MSG_DELETE_VEHICLE, vehicle));
+}
+
+ClientComms.prototype.sendUpdateVehicle = function(vehicle) {
+    this.server.broadcast(Message.constructMessageJSON(MSG_UPDATE_VEHICLE, vehicle));
+}
+
+ClientComms.prototype.sendTelemetry = function(telemetry) {
+    this.server.broadcast(Message.constructMessageJSON(MSG_VEHICLE_TELEMETERY, vehicle));
+}
+
+ClientComms.prototype.sendPayload = function(payload) {
+    this.server.broadcast(Message.constructMessageJSON(MSG_VEHICLE_PAYLOAD, vehicle));
+}
+
+fireNewConnection = function(self, connection) {
+    self.emit('newConnection', connection);
+}
+
+rcvdAddVehicle = function(self, data) {
+    self.emit('addVehicle', data);
+}
+
+rcvdDeleteVehicle = function(self, data) {
+    self.emit('deleteVehicle', data);
+}
+
+rcvdUpdateVehicle = function(self, data) {
+    self.emit('updateVehicle', data);
+}
+
+rcvdSendVehicles = function(self, connection) {
+    self.emit('sendVehicles', connection);
+}
+
+rcvdPerformTest = function(self, data) {
+    self.emit('vehicleTest', data);
+}
+
+rcvdLand = function(self, data) {
+    self.emit('vehicleLand', data);
+}
+
+rcvdAbort = function(self, data) {
+    self.emit('vehicleAbort', data);
+}
+
+rcvdLaunch = function(self, data) {
+    self.emit('vehicleLaunch', data);
+}
+
+rcvdUp = function(self, data) {
+    self.emit('vehicleUp', data);
+}
+
+rcvdDown = function(self, data) {
+    self.emit('vehicleDown', data);
+}
+
+rcvdLeft = function(self, data) {
+    self.emit('vehicleLeft', data);
+}
+
+rcvdRight = function(self, data) {
+    self.emit('vehicleRight', data);
+}
+
+rcvdForward = function(self, data) {
+    self.emit('vehicleForward', data);
+}
+
+rcvdReverse = function(self, data) {
+    self.emit('vehicleReverse', data);
+}
+
+rcvdTurnLeft = function(self, data) {
+    self.emit('vehicleTurnLeft', data);
+}
+
+rcvdTurnRight = function(self, data) {
+    self.emit('vehicleTurnRight', data);
+}
+
+/*
+rcvd = function(self, data) {
+    self.emit('vehicle', data);
+}
+*/
+
 
 function originIsAllowed(origin) {
     // put logic here to detect whether the specified origin is allowed
     return true;
 }
 
-wsServer.on('request', function(request) {
+
+function processConnectionAttempt(self, request) {
     console.log((new Date()) + ' Connection attempt from origin ' + request.origin +
 	      ', websocket ver: ' + request.webSocketVersion + 
 	      ', protocols : ' + request.requestedProtocols.length + 
@@ -64,7 +185,6 @@ wsServer.on('request', function(request) {
 	console.log((new Date()) + ' Connection rejected, invalid origin: ' + request.origin);
 	return;
     }
-
 
     var connection = null;
 
@@ -84,56 +204,72 @@ wsServer.on('request', function(request) {
     }
 
     console.log((new Date()) + ' Connection accepted, protocol: ' + connection.protocol);
-    connection.on('message', function(message) {
-	if (message.type === 'utf8') {
-	    console.log((new Date()) + ' Received message: ' + message.utf8Data);
-	    msg = JSON.parse(message.utf8Data);
-	    console.log((new Date()) + '                   id:  ' + msg.id);
-	    console.log((new Date()) + '                   msg: ' + msg.msg);
 
-	    processMessage(msg.id, msg.msg);
+    // add the message listener
+    connection.on('message', function(message) {processRawMessage(self, connection, message);});
 
-	    // the following is purely for testing 
-	    // connection.sendUTF(JSON.stringify({id: 'ack'}));
+    // add the connection close listener
+    connection.on('close', processConnectionClosed);
 
-	}
-	else if (message.type === 'binary') {
-	    console.log((new Date()) + ' Received binary message of ' + message.binaryData.length + ' bytes');
-	    connection.sendBytes(message.binaryData);
-	}
-    });
-    connection.on('close', function(reasonCode, description) {
-	console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-});
+    // fire the connection event
+    fireNewConnection(self, connection);
+}
 
-var MSG_ADD_VEHICLE = 'addVehicle';
-function processMessage(id, body) {
+function processConnectionClosed(reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+}
+
+function processRawMessage(self, connection, message) {
+    if (message.type === 'utf8') {
+	console.log((new Date()) + ' Received message: ' + message.utf8Data);
+
+	// msg = JSON.parse(message.utf8Data);
+
+	// deconstruct the message
+	msg = Message.deconstructMessage(message.utf8Data);
+
+	console.log((new Date()) + '          msg:  ' + msg);
+	console.log((new Date()) + '          id:  ' + msg.id);
+	console.log((new Date()) + '          msg: ' + msg.msg);
+
+	processMessage(self, connection, msg.id, msg.msg);
+
+	// the following is purely for testing 
+	// connection.sendUTF(JSON.stringify({id: 'ack'}));
+
+    }
+    else if (message.type === 'binary') {
+	console.log((new Date()) + ' Received binary message of ' + message.binaryData.length + ' bytes');
+/	// connection.sendBytes(message.binaryData);
+    }
+}
+
+function processMessage(self, connection, id, body) {
+    // TODO: this presumes that the body is always json stringified, should this be so?
     var msg = JSON.parse(body);
 
     switch(id) {
 	case MSG_ADD_VEHICLE:
-	    addVehicle(msg);
+	    // addVehicle(msg);
+            rcvdAddVehicle(self, msg);
 	    break;
 
 	case MSG_DELETE_VEHICLE:
-	    deleteVehicle(msg);
+	    rcvdDeleteVehicle(self, msg);
 	    break;
 
 	case MSG_UPDATE_VEHICLE:
-	    updateVehicle(msg);
+	    rcvdUpdateVehicle(self, msg);
 	    break;
 
 	case MSG_GET_VEHICLES:
-	    sendVehicles();
+	    rcvdSendVehicles(self, connection);
 	    break;
 
 	case MSG_GET_TELEMETERY:
-	    sendTelemetery();
 	    break;
 
 	case MSG_GET_PAYLOAD:
-	    sendPayload();
 	    break;
 
 	case MSG_CMD_EMERGENCY_STOP:
@@ -156,88 +292,3 @@ function processMessage(id, body) {
     }
 }
 
-function addVehicle(msg) {
-    var i = vehicles.length;
-    vehicles[i] = new Vehicle(msg);
-    console.log((new Date()) + ' Adding vehicle ' + msg.name);
-}
-
-function deleteVehicle(msg) {
-    // if the message isn't set and the name isn't set then do nothing
-    if(!msg && !msg.name) {
-        console.log((new Date()) + ' Delete vehicle failed, msg.name is not specififed.');
-	return;
-    }
-
-    // find the vehicle
-    var position = findVehicle(msg.name);
-
-    // remove from the array if found
-    if(position >= 0) {
-        vehicles.splice(position, 1);
-    } else {
-        console.log((new Date()) + ' Delete vehicle failed, vehicle not found for: ' + msg.name);
-    }
-}
-
-function updateVehicle(msg) {
-    // if the message isn't set and the name isn't set then do nothing
-    if(!msg && !msg.name) {
-        console.log((new Date()) + ' Update vehicle failed, msg.name is not specififed.');
-	return;
-    }
-
-    // find the vehicle
-    var position = findVehicle(msg.name);
-
-    if(position >= 0) {
-        vehicles[position] = new Vehicle(msg);
-    } else {
-        console.log((new Date()) + ' Update vehicle failed, vehicle not found for: ' + msg.name);
-    }
-}
-
-/** 
- * find Vehicle - finds the vehicle based on it's name
- * 
- * returns -1 if not found, otherwise the position in the vehicles array
- */
-function findVehicle(name) {
-    var position = -1;
-
-    // if name isn't set then return
-    if(!name) {
-	return position;
-    }
-
-    for(var i = 0, l = vehicles.length; i < l; i++) {
-	if(vehicles[i].name === name) {
-	    position = i;
-	    break;
-	}
-    }
-
-    return position;
-}
-
-/** 
- * send Vehicles - send all the vehicles 
- */
-function sendVehicles() {
-}
-
-/** 
- * send Telemetery - send the telemetery 
- * 
- * vehicleNbr   the vehicle to send telemetery for, if specified
- */
-function sendTelemetery(vehicleNbr, data) {
-}
-
-/** 
- * send Payload - send the most recent payload information
- * 
- * vehicleNbr   the vehicle to send payload for, if specified
- */
-function sendPayload(vehicleNbr) {
-}
--- videre_server.js
+++ videre_server.js
@@ -17,39 +17,219 @@
  */
 
 var videre_comms  = require('./videre-common/js/videre_comms.js');
-var vehicle       = require('./videre-common/js/vehicle.js');
-var client_comms  = require('./client_comms.js');
-// var vehicle_comms = require('./vehicle_comms.js');
+// var vehicle       = require('./videre-common/js/vehicle.js');
+var ClientComms   = require('./client_comms.js');
+var fs            = require('fs');
 
-// var RemoteVehicle = require('./vehicle_comms.js').RemoteVehicle;
-// var parrot = require('./parrotArDrone.js').ParrotARDrone;
-
-// var test = exports;
-// exports.Parrot = require('./parrotArDrone.js');
 var Parrot = require('./vehicle/parrotArDroneV1.js');
 
-// var remoteVehicle = parrotARDrone.createVehicle();
-// var remoteVehicle = new test.Parrot({address: "192.168.1.3"});
-var remoteVehicle = new Parrot({address: "192.168.1.3"});
+eval(fs.readFileSync('./videre-common/js/vehicle.js').toString());
 
-remoteVehicle.on('telemetry', function(d) {processTelemetry(d);});
+var VEHICLES_FILE = 'vehicles.json';
+// load the vehicle configs from the file
+var vehicles = loadVehicles(VEHICLES_FILE);
 
-remoteVehicle.testRun();
+// setup the client communications
+clientComms = new ClientComms();
 
+// listen for the events from clients
+clientComms.on('addVehicle', function(d) {addVehicle(d);});
+clientComms.on('deleteVehicle', function(d) {deleteVehicle(d);});
+clientComms.on('updateVehicle', function(d) {updateVehicle(d);});
+clientComms.on('sendVehicles', function(c) {sendVehicles(c);});
+clientComms.on('newConnection', function(c) {newConnection(c);});
+/*
+clientComms.on('vehicleTest', function(d) {x(d);});
+clientComms.on('vehicleLand', function(d) {x(d);});
+clientComms.on('vehicleAbort', function(d) {x(d);});
+clientComms.on('vehicleLaunch', function(d) {x(d);});
+clientComms.on('vehicleUp', function(d) {x(d);});
+clientComms.on('vehicleDown', function(d) {x(d);});
+clientComms.on('vehicleLeft', function(d) {x(d);});
+clientComms.on('vehicleRight', function(d) {x(d);});
+clientComms.on('vehicleForward', function(d) {x(d);});
+clientComms.on('vehicleReverse', function(d) {x(d);});
+clientComms.on('vehicleTurnLeft', function(d) {x(d);});
+clientComms.on('vehicleTurnRight', function(d) {x(d);});
+*/
+
+// start up the server for clients
+clientComms.startClientServer();
+
+// startup the comms with the remote vehicles
+var remoteVehicles = startVehicleComms(vehicles);
+
+for(i = 0, l = remoteVehicles.length; i < l; i++) {
+    remoteVehicles[i].on('telemetry', function(d) {processTelemetry(d);});
+    remoteVehicles[i].testRun();
+}
+
+function startVehicleComms(vehicles) {
+    var vehicleComms = new Array();
+    var remoteVehicle = null;
+
+    for(i = 0, l = vehicles.length; i < l; i++) {
+        console.log("startVehicleComms: loading " + vehicles[i].name);
+
+	remoteVehicle = null;
+
+	switch(vehicles[i].deviceType) {
+	    case(VEHICLE_DEVICE_PARROT_V1):
+		// create the remote vehicle comms handler and map values from the common vehicle data
+		remoteVehicle = new Parrot({
+		    name: vehicles[i].name, 
+		    address: "192.168.1.3"
+	        });
+		break;
+
+	    // not implemented yet 
+	    case(VEHICLE_DEVICE_PARROT_V2):
+		break;
+
+	    // unknown device type
+	    default:
+		console.log('videre-server: vehicle device ' + vehicles[i].deviceType + ' not supported for vehicle ' + vehicles[i].name);
+		break;
+	}
+
+	if (remoteVehicle) {
+            vehicleComms.push(remoteVehicle);
+
+	    remoteVehicle.on('telemetry', function(d) {processTelemetry(d);});
+	    remoteVehicle.on('payload', function(d) {processPayload(d);});
+	}
+    }
+
+    return vehicleComms;
+}
+
+/* 
+ * load vehicles
+ */
+function loadVehicles(filename) {
+    var data = fs.readFileSync(filename);
+
+    var dataJSON = JSON.parse(data);
+
+    // the received JSON data will be object, not vehicle instances, so convert
+    // may not need to do this as there is no prototype on Vehicles, so could be treated just as an object...
+    var vehicles = new Array();
+
+    for(i = 0, l = dataJSON.length; i < l; i++) {
+        vehicles.push(new Vehicle(dataJSON[i]));
+    }
+
+    return vehicles;
+}
+
+/* 
+ * save vehicles
+ */
+function saveVehicles(filename) {
+    fs.writeFileSync(filename, JSON.stringify(vehicles, null, '\t'));
+}
+
+// add a watchdog to check if vehicles get add
+function addVehicle(msg) {
+    console.log((new Date()) + ' Adding vehicle ' + msg.name);
+    vehicles.push(new Vehicle(msg));
+
+    saveVehicles(VEHICLES_FILE);
+
+    clientComms.sendAddVehicle(msg);
+}
+
+function deleteVehicle(msg) {
+    // if the message isn't set and the name isn't set then do nothing
+    if(!msg && !msg.name) {
+        console.log((new Date()) + ' Delete vehicle failed, msg.name is not specififed.');
+	return;
+    }
+
+    // find the vehicle
+    var position = findVehicle(msg.name);
+
+    // remove from the array if found
+    if(position >= 0) {
+        vehicles.splice(position, 1);
+    } else {
+        console.log((new Date()) + ' Delete vehicle failed, vehicle not found for: ' + msg.name);
+    }
+
+    saveVehicles(VEHICLES_FILE);
+}
+
+function updateVehicle(msg) {
+    // if the message isn't set and the name isn't set then do nothing
+    if(!msg && !msg.name) {
+        console.log((new Date()) + ' Update vehicle failed, msg.name is not specififed.');
+	return;
+    }
+
+    // find the vehicle
+    var position = findVehicle(msg.name);
+
+    if(position >= 0) {
+        vehicles[position] = new Vehicle(msg);
+    } else {
+        console.log((new Date()) + ' Update vehicle failed, vehicle not found for: ' + msg.name);
+    }
+
+    saveVehicles(VEHICLES_FILE);
+}
+
+function sendVehicles(connection) {
+    clientComms.sendVehicles(connection, vehicles);
+}
+
+function newConnection(connection) {
+    // on a new connection send the vehicles to the client
+    clientComms.sendVehicles(connection, vehicles);
+}
+
+/*
+ * process telemetry
+ *
+ * capture telemetry and pass to the clients
+ *
+ * note: some devices may pass payload and telemetry together
+ */
 function processTelemetry(d) {
     console.log('videre-server: telemetry...');
     console.log(d);
 }
 
-// add a watchdog to check if vehicles get added
+/*
+ * process payload
+ *
+ * capture payload and pass to the clients
+ *
+ * note: some devices may pass payload and telemetry together
+ */
+function processPayload(d) {
+    console.log('videre-server: payload...');
+    console.log(d);
+}
 
+/** 
+ * find Vehicle - finds the vehicle based on it's name
+ * 
+ * returns -1 if not found, otherwise the position in the vehicles array
+ */
+function findVehicle(name) {
+    var position = -1;
 
-// if a vehicle is added then check to see if it should be configured
+    // if name isn't set then return
+    if(!name) {
+	return position;
+    }
 
+    for(var i = 0, l = vehicles.length; i < l; i++) {
+	if(vehicles[i].name === name) {
+	    position = i;
+	    break;
+	}
+    }
 
-// capture telemetry and pass to the clients
-
-
-// capture payload and pass to the clients
-
-// note: some devices may pass payload and telemetry together
+    return position;
+}
